928c58a6075714448d9bcd029cbffb00a0c27f8c
panviktar <panviktar@gmail.com>
2021-03-28 19:50:23 +0300

    feat:transform-ar function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === "--discard-next") {
+        newArr.push("delete");
+        i++;
+      } else if (arr[i] === "--discard-prev") {
+        newArr.pop();
+      } else if (arr[i] === "--double-next") {
+        if (i !== arr.length - 1) {
+          newArr.push(arr[i + 1]);
+        }
+      } else if (arr[i] === "--double-prev") {
+        if (i !== 0) {
+          newArr.push(newArr[newArr.length - 1]);
+        }
+      } else newArr.push(arr[i]);
+    }
+    return newArr.filter((item) => item !== "delete");
+}
