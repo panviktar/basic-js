ca4355f37ccf8c7d74d0a0c74b48c8aa7c37f46c
panviktar <panviktar@gmail.com>
2021-03-29 19:25:04 +0300

    feat: carbon function exceptions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+function dateSample(sampleActivity) {
+  if (
+      typeof sampleActivity !== "string" ||
+      +sampleActivity > 15 ||
+      +sampleActivity < 1 ||
+      !isFinite(+sampleActivity)
+  )
+    return false;
+  let k
+  let t
+  if (t < 0) return false;
+  return t;
+}
